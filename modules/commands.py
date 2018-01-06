c848f619137867eecb8f846148b2121f5490e0eb
Cory Smith <cjsmith@godaddy.com>
2018-01-05 18:56:52 -0700

    Added a couple commands- Added an - infront of the details of command so it can be defined cleaner in !help

--- modules/commands.py
+++ modules/commands.py
@@ -10,7 +10,7 @@ class Commands():
 
     @commands.command(aliases=['raidtime'])
     async def RaidTime(self, ctx):
-        """Tells the user for the 100th time when raids are."""
+        """-Tells the user for the 100th time when raids are."""
         await ctx.channel.send("Raids are on the following days:")
         await ctx.channel.send("Thursday: 830pm Eastern")
         await ctx.channel.send("Sunday: 730pm Eastern")
@@ -18,31 +18,31 @@ class Commands():
 
     @commands.command(aliases=['areyoulistening'])
     async def AreYouListening(self, ctx):
-        """Uhhhhh....hellooooo"""
+        """-Uhhhhh....hellooooo"""
         await ctx.channel.send("I swear to god, I don't know why I even bother, no one listens anyways.")
 
-    @commands.commmand(aliases=['twitchchannel'])
+    @commands.command(aliases=['twitchchannel'])
     async def TwitchChannel(self, ctx):
-        """Holy crap are you even paying attention when I talk?"""
+        """-Holy crap are you even paying attention when I talk?"""
         await ctx.channel.send("https://www.twitch.tv/elimere")
 
     @commands.command(aliases=['mods'])
     async def Mods(self, ctx):
-        """These are the mods required for raiding"""
+        """-These are the mods required for raiding"""
         await ctx.channel.send("Again, here are the mods required for the 100000th time.")
         await ctx.channel.send("http://www.mediafire.com/file/gpc0t8yjhi5369f/Booty_Bay_Surf_Club_Raid_Pack.ccip")
         await ctx.channel.send("You need the Twitch app installed, download this, then run it.")
 
     @commands.command(aliases=['Silv'])
     async def Silv(self, ctx):
-        """Damn it Silv!"""
+        """-Damn it Silv!"""
         await ctx.channel.send("Silv you can't cloak everything damn it!")
 
-    @commands.command(aliases=['void elfs'])
+    @commands.command(aliases=['voidelfs'])
     async def Silv(self, ctx):
-        """DTell us how you REALLY feel about Void Elves"""
+        """-Tell us how you REALLY feel about Void Elves"""
         await ctx.channel.send("Well first, fuck them")
-        await ctx.channel.send("Second..i secretly love their hair")
+        await ctx.channel.send("Second..I secretly envy their hair")
 
 
 
