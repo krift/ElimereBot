410c761d7a9df36f4b4100374480a9e633bdc772
krift <mastec0890@gmail.com>
2018-01-08 14:52:55 -0500

    Updated sample config with server and channel id variables

--- config-sample.py
+++ config-sample.py
@@ -1,4 +1,6 @@
 token = "sample token file here"
 twitchBotId = "used in the BotRespond command"
 warcraftLogsAPI = "used to access warcraft logs"
+serverDiscId = "this should be an int, not a string"
+raidlogsChannelId = "this should be an int, not a string"
 """Rename this file to config.py and insure your .gitignore has config.py in it"""
