45c29486615caf987a6f11052b1f0bee468f4757
krift <mastec0890@gmail.com>
2018-01-14 13:34:44 -0500

    Testing github stuff

--- MainBotFile.py
+++ MainBotFile.py
@@ -56,9 +56,12 @@ class ElimereBot(commands.AutoShardedBot):
         await channel.send("Hello "+member.mention+"! Hope you enjoy your stay here! We're all happy you decided to join us!")
 
     async def on_message(self, message):
-        embeds = message.embeds[0].to_dict()
-        print(embeds)
-        print(embeds['author']['title'])
+        if message.embeds:
+            print("True")
+            if message.author.name == "GitHub":
+                embeds = message.embeds[0].to_dict()
+                if embeds['title'].lower().rfind('elimerebot:master'):
+                    print("Found it")
         if message.author.bot is False:  # So the bot won't process bot messages
             if message.content == '':
                 return
