bec5627fab6dbf63d010eb9c12f89270c65ecee5
krift <mastec0890@gmail.com>
2018-01-19 12:28:32 -0500

    Bot now commits local changes then pulls

--- modules/dev.py
+++ modules/dev.py
@@ -39,7 +39,7 @@ class Dev:
         channel = self.bot.get_guild(config.guildServerID).get_channel(config.guildLogChanID)
         await channel.send("Oh boy! Looks like I need to update myself!")
         g = git.cmd.Git(path)
-        g.stash()
+        g.commit()
         g.pull()
         await channel.send("All updated! Now I need to restart! Be right back guys!")
         os.system('sudo systemctl restart elimerebot.service')
