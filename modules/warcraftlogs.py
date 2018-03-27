95e755f6f46e94a5e0bc1b8bec360d77fbe7c543
Brian <mastec0890@gmail.com>
2018-03-27 12:37:18 -0400

    Removed unused import from database.py Small changes made to warcraftlogs.py output if there is no new log to display

--- modules/warcraftlogs.py
+++ modules/warcraftlogs.py
@@ -15,8 +15,9 @@ class WarcraftLogs:
     async def PullNewLog(self, ctx):
         """This will pull the latest log from warcraft logs if there is one to pull"""
         log = await func.CheckForLogs()
+        channel = self.bot.get_guild(config.guildServerID).get_channel(config.guildLogChanID)
         if log is None:
-            return
+            await channel.send("No new logs available.")
         else:
             e = discord.Embed(title=log['title'], colour=discord.Colour.purple())
             e.add_field(name='Zone', value=botoptions.zones.get(log['zone'], 'Unknown Zone'))
@@ -24,7 +25,6 @@ class WarcraftLogs:
             e.timestamp = datetime.datetime.fromtimestamp(log['start'] / 1e3)
             e.set_thumbnail(url="https://www.method.gg/images/raidrace/antorus-argus-28-11-17-05-32-08-1.jpg")
             e.set_image(url="https://s3.amazonaws.com/file3.guildlaunch.net/462275/tabard.png")
-            channel = self.bot.get_guild(config.guildServerID).get_channel(config.guildLogChanID)
             await channel.send(embed=e)
 
     @commands.command(aliases=['logbydate, showlogbydate'])
